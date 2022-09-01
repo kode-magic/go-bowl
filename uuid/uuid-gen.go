b815e567ea562d37583cc023ea2a8f4a7021eeaa
kode-magic <mohamedjramsey@gmail.com>
2022-09-01 19:35:29 +0000

    convert uuid to string

--- uuid/uuid-gen.go
+++ uuid/uuid-gen.go
@@ -22,5 +22,12 @@ func ConvertToUUID(id string) uuid.UUID {
 	return ID
 }
 
+func ConvertToString(id uuid.UUID) (string, error) {
+	theId, err := uuid.Parse(id.String())
+	if err != nil {
+		return "", err
+	}
 
+	return theId.String(), nil
+}
 
