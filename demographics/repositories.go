e278da7f34e00fe41fd1b40724aedcab4a538d5b
kode-magic <mohamedjramsey@gmail.com>
2022-09-02 22:11:30 +0000

    demographics gorm add region method

--- demographics/repositories.go
+++ demographics/repositories.go
@@ -23,3 +23,38 @@ func ListCountries() (*[]Country, error) {
 	return &countries, nil
 }
 
+func AddRegion(region *Region) (*Region, error) {
+	db, err := ConnectGormDB()
+	CheckErr(err)
+	createErr := db.Create(&region).Error
+	if createErr != nil {
+		return nil, createErr
+	}
+
+	return region, nil
+}
+
+func ListCountryRegions(country string) (*[]Region, error) {
+	db, err := ConnectGormDB()
+	CheckErr(err)
+	var regions []Region
+	fetchErr := db.Preload("Country").Where("country_id = ?", country).Find(&regions).Error
+	if fetchErr != nil {
+		return nil, fetchErr
+	}
+
+	return &regions, nil
+}
+
+func GetRegionById(id string) (*Region, error) {
+	db, err := ConnectGormDB()
+	CheckErr(err)
+	var region Region
+	
+	fetchErr := db.Preload("Country").Where("id = ?", id).Take(&region).Error
+	if fetchErr != nil {
+		return nil, fetchErr
+	}
+
+	return &region, nil
+}
