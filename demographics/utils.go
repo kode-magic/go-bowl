9de9352933e5559cb46815df31b8f72746620a2f
kode-magic <mohamedjramsey@gmail.com>
2022-09-02 22:08:16 +0000

    demographics gorm db connection

--- demographics/utils.go
+++ demographics/utils.go
@@ -3,15 +3,27 @@ package demographics
 import (
 	"database/sql"
 	"log"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func connectDemograpgicsDB() *sql.DB {
 	db, err := sql.Open("sqlite3", "./demographics/demographics.db")
 	CheckErr(err)
-	// defer close
-	// defer db.Close()
 
 	return db
+}
+
+func ConnectGormDB() (*gorm.DB, error){
+	db, err := gorm.Open(sqlite.Open("./demographics/demographics.db"), &gorm.Config{})
+	CheckErr(err)
+
+	db.AutoMigrate(
+		&Country{},
+		&Region{},
+		&District{},
+	)
+  return db, nil
 }
 
 func CheckErr(err error) {
