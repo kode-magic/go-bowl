3475a5bb21485c08ed60dcbeddc53e594be217a1
kode-magic <mohamedjramsey@gmail.com>
2022-09-09 15:36:07 +0000

    chiefdoms by district

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/kode-magic/go-bowl/demographics"
 )
 
 func main() {
 	fmt.Println("*******************************************************************************")
 	fmt.Println("\t\t\t\tGO BOWL")
+	demographics.DistrictsByRegion("Western")
 	fmt.Println("*******************************************************************************")
 }
