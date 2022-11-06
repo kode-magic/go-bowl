057ff3c215e93940fead9170df2f422281db6acf
kode-magic <mohamedjramsey@gmail.com>
2022-11-06 15:34:03 +0000

    Create academic.go
    rabbitmq consumer1 example

---
+++ rabbit/examples/consumers/academic.go
@@ -0,0 +1,42 @@
+package main
+
+import "github.com/kode-magic/go-bowl/rabbit"
+
+func main() {
+	exchange := rabbit.ExchangeConfig{
+		Exchange:   "school_space",
+		Kind:       "fanout",
+		Durable:    true,
+		AutoDelete: false,
+		Internal:   false,
+		NoWait:     false,
+	}
+	queue := rabbit.QueueConfig{
+		Exchange:   "school_space",
+		RoutingKey: "",
+		QueueName:  "",
+		Durable:    false,
+		AutoDelete: false,
+		Exclusive:  true,
+		NoWait:     false,
+	}
+	rabbitMQ := rabbit.MQConfig{
+		Host:     "localhost",
+		Port:     "5672",
+		User:     "guest",
+		Password: "guest",
+	}
+	consumer := rabbit.ConsumerConfig{
+		Consumer:  "academic",
+		AtoAck:    true,
+		Exclusive: false,
+		NoLocal:   false,
+		NoWait:    false,
+		Args:      nil,
+	}
+
+	publisher := rabbit.NewPublisher(&rabbitMQ)
+	publisher.SetupExchange(&exchange)
+	q := publisher.SetupQueue(&queue)
+	publisher.Consumers(&consumer, q)
+}
