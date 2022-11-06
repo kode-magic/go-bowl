c1727eea351ed739d39e7496b2cf6ed0f69542d8
kode-magic <mohamedjramsey@gmail.com>
2022-11-06 15:32:27 +0000

    Update main.go
    testing rabbit instances

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/kode-magic/go-bowl/demographics"
 )
 
 func main() {
 	fmt.Println("*******************************************************************************")
 	fmt.Println("\t\t\t\tGO BOWL")
-	demographics.RegionChiefdoms("Western")
-	fmt.Println("*******************************************************************************")
 }
