8a8ccb0a2fde964a7f4abf0ee9240dc996927c67
kode-magic <mohamedjramsey@gmail.com>
2022-11-19 07:53:33 +0000

    Update main.go
    test packages

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/kode-magic/go-bowl/base64"
+	"github.com/kode-magic/go-bowl/validator"
 )
 
 func main() {
 	fmt.Println("*******************************************************************************")
 	fmt.Println("\t\t\t\tGO BOWL")
+	fmt.Println("phone: ", validator.IsPhoneNumber("23288110178"))
+	fmt.Println("email: ", validator.IsEmail("james@medram.dev"))
+	fmt.Println("encoded: ", base64.EncodeString("james@medram.dev"))
+	fmt.Println("decoded", base64.DecodeString("amFtZXNAbWVkcmFtLmRldg=="))
 }
