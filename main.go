c0f326cc59dc2b4140b809f65e2d0c8717115ae3
kode-magic <mohamedjramsey@gmail.com>
2022-11-20 04:32:10 +0000

    jwt tester
    text jwt for email generator

--- main.go
+++ main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/kode-magic/go-bowl/base64"
-	"github.com/kode-magic/go-bowl/validator"
+	"github.com/kode-magic/go-bowl/jwt"
+	"time"
 )
 
 func main() {
 	fmt.Println("*******************************************************************************")
 	fmt.Println("\t\t\t\tGO BOWL")
-	fmt.Println("phone: ", validator.IsPhoneNumber("23288110178"))
-	fmt.Println("email: ", validator.IsEmail("james@medram.dev"))
-	fmt.Println("encoded: ", base64.EncodeString("james@medram.dev"))
-	fmt.Println("decoded", base64.DecodeString("amFtZXNAbWVkcmFtLmRldg=="))
+	fmt.Println("token: ", jwt.GenerateJWTForEmail("james@medram.dev", time.Hour*24))
 }
