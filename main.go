e66fa351fc364f1d6e31389b6143dac03e2ef8cd
kode-magic <mohamedjramsey@gmail.com>
2022-11-21 06:27:35 +0000

    Update main.go
    test

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/kode-magic/go-bowl/bcrypt"
 	"github.com/kode-magic/go-bowl/jwt"
 	"time"
 )
@@ -9,5 +10,8 @@ import (
 func main() {
 	fmt.Println("*******************************************************************************")
 	fmt.Println("\t\t\t\tGO BOWL")
+	hash, _ := bcrypt.HashPassword("Kodemagik!!1")
+	fmt.Println("hashes: ", hash)
+	fmt.Println("compare: ", bcrypt.CheckPasswordHash("Kodemagik!!1", hash))
 	fmt.Println("token: ", jwt.GenerateJWTForEmail("james@medram.dev", time.Hour*24))
 }
